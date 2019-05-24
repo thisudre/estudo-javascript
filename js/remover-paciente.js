98a77b2b06fa4cdf24155cc206263301dbd0df5a
Thiago Sudré Cezarino <thiagoty@gmail.com>
2019-05-24 15:00:45 -0300

    adição de efeito de remoção

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -12,5 +12,10 @@ var tabela = document.querySelector("table");
 
 tabela.addEventListener("dblclick", function(event)
 {
-    event.target.parentNode.remove();
+    event.target.parentNode.classList.add("fade-out");
+    
+    setTimeout(function()
+    {
+        event.target.parentNode.remove();
+    }, 500)
 })
